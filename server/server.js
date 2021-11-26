73dad4820297ee96ea6f5cccbd1dd0d5fe906d14
Galatia <ptrivedius@aol.com>
2021-11-26 15:15:22 -0500

    pushing code

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-import authMiddleware from './utils/auth';
-
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
